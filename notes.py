56b0979ddfd0805c898a458796b8e9a9448cbe9f
Benjamin Johnson <bjohnson@fcusd.org>
2024-09-09 19:33:32 -0700

    Create notes.py

---
+++ notes.py
@@ -0,0 +1,48 @@
+# Conditional Statements #
+#**Learning objectives**
+#
+#After this section:
+#
+# * You will be able to use a simple conditional statement in programming
+# * You will know what a Boolean value is
+# * You will be able to express conditionals with comparison operators
+
+#|Operator	| Purpose					| Example	|
+#|==			| Equal to					| a == b	|
+#|!=			| Not equal to				| a != b	|
+#|>			| Greater than				| a > b		|
+#|>=			| Greater than or equal to	| a >= b	|
+#|<			| Less than					| a < b		|
+#|<=			| Less than or equal to		| a <= b	|
+
+
+## Live Demo ##
+# if statements
+#age = int(input("How old are you? "))
+#if age > 17:
+#    print("You are of age!")
+#    print("Here's a copy of GTA6 for you.")
+#print("Next customer, please!")
+#
+#number = int(input("Please type in a number: "))
+#
+#if number < 0:
+#    print("The number is negative.")
+#
+#if number > 0:
+#    print("The number is positive.")
+#
+#if number == 0:
+#    print("The number is zero.")
+#
+# Boolean type
+#a = 3
+#condition = a < 5
+#print(condition)
+#if condition:
+#    print("a is less than 5")
+#
+#condition = True
+#if condition:
+#    print("This is printed every time.")
+#
